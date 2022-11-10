1bfd2df03c67115f827a50ba0c7b0f4c9a8097a6
woistkeller <caiankardoso@gmail.com>
2022-11-10 14:49:51 -0300

    glosbe translations request improvement

--- src/main/server/glosbe.js
+++ src/main/server/glosbe.js
@@ -5,7 +5,8 @@ const cheerio = require('cheerio')
 const { setupCache } = require('axios-cache-adapter')
 
 const cache = setupCache({
-  maxAge: 604800000 // 7 days in milliseconds
+  maxAge: 1000 * 60 * 60 * 24 * 7 // 7 days in milliseconds, however, i am saving it nowhere,
+  // so, when the app is closed, bum, bye bye cache
 })
 
 const glosbe = axios.create({
@@ -14,7 +15,7 @@ const glosbe = axios.create({
 
 // TODO: improve that, now i know how to make a proper promise
 const glosbeWords = async (language, word) => {
-  return new Promise((resolve) => {
+  return new Promise((resolve, reject) => {
     const userLanguage = shortenLanguage(database.user.language)
     const url = encodeURI(
       `https://glosbe.com/${language}/${userLanguage}/${word.toLowerCase()}`
@@ -29,31 +30,48 @@ const glosbeWords = async (language, word) => {
         content = content.filter((translation) => translation.length > 0)
 
         if (content.length) resolve(content)
-        else throw Error('Just being a little silly 😎') // ok, i did it because i don't think it matters
+        else throw Error()
       })
       .catch(() => {
-        resolve(false)
+        reject(Error("Couldn't found any definition."))
       })
   })
 }
 
-const glosbeTranslate = async (language, text) => {
+const glosbeTranslate = async (language, word) => {
   return new Promise((resolve, reject) => {
     const userLanguage = shortenLanguage(database.user.language)
-    const url = encodeURI(
-      `https://translator-api.glosbe.com/translateByLangDetect?sourceLang=${language}&targetLang=${userLanguage}`
-    )
+
     glosbe
-      .post(url, text, {
-        headers: {
-          'Content-Type': 'text/plain'
+      .post(
+        'https://translator-api.glosbe.com/translateByLangWithScore',
+        word,
+        {
+          params: {
+            sourceLang: language,
+            targetLang: userLanguage
+          },
+          headers: {
+            Accept: '*/*',
+            'Accept-Language': 'en-US,en;q=0.5',
+            'Accept-Encoding': 'gzip, deflate, br',
+            Referer: 'https://glosbe.com/',
+            'Content-Type': 'text/plain;charset=UTF-8',
+            Origin: 'https://glosbe.com',
+            Connection: 'keep-alive',
+            'Sec-Fetch-Dest': 'empty',
+            'Sec-Fetch-Mode': 'cors',
+            'Sec-Fetch-Site': 'same-site',
+            'Sec-GPC': '1',
+            TE: 'trailers'
+          }
         }
-      })
+      )
       .then((response) => {
         resolve(response.data.translation)
       })
       .catch(() => {
-        reject(Error)
+        reject(Error('Error while looking for translations.'))
       })
   })
 }
